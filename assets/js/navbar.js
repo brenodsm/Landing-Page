bd4fb8b542070ca883f0a3cbefbafeae58e1c311
brenodsm <breno.dsmnb@gmail.com>
2024-12-17 23:30:07 -0300

    feat: finalizar implementação do menu hamburguer

---
+++ assets/js/navbar.js
@@ -0,0 +1,18 @@
+const navbar = document.querySelector(".mobile-menu");
+const nav = document.querySelector("nav");
+const body = document.querySelector("body");
+
+navbar.addEventListener("click", activeMenu);
+
+function activeMenu() {
+  nav.classList.toggle("active");
+}
+
+body.addEventListener("click", closeMenuOnOutsideClick);
+
+function closeMenuOnOutsideClick(event) {
+  // Verifica se o clique NÃO foi no nav ou em seus filhos e também fora da navbar
+  if (!nav.contains(event.target) && !navbar.contains(event.target)) {
+    nav.classList.remove("active");
+  }
+}
